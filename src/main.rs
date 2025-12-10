4ee9d3f80ec8c3c237f6e3ee6a564f95417de4d2
Jon Knox <me@jonknox.com>
2025-12-10 16:16:23 +0000

    feat:cpu

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::alloc::System;
 
 use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
 use ratatui::{
-    DefaultTerminal, Frame, layout::{self, Constraint, Layout}, style::Stylize, text::Line, widgets::{Block, Paragraph}
+    DefaultTerminal, Frame, layout::{self, Constraint, Layout}, style::{Style, Stylize}, symbols, text::Line, widgets::{Axis, Block, Chart, Dataset, GraphType, Paragraph}
 };
 
 fn main() -> color_eyre::Result<()> {
@@ -19,7 +19,7 @@ pub struct App {
     /// Is the application running?
     running: bool,
     system: sysinfo::System,
-    cpu: Vec<(f64,f32)>
+    cpu: Vec<(f64,f64)>
 }
 
 impl App {
@@ -36,11 +36,11 @@ impl App {
     pub fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
         self.running = true;
         while self.running {
-
-            // See: +41:00 @ https://www.youtube.com/watch?v=OkmYsa25pIw
-
-            //self.cpu.
-            terminal.draw(|frame| self.render(frame))?;
+            self.system.refresh_cpu_all();
+            terminal.draw(|frame| {
+                self.cpu.push((frame.count() as f64, self.system.global_cpu_usage() as f64));
+                self.render(frame)
+            })?;
             self.handle_crossterm_events()?;
         }
         Ok(())
@@ -60,7 +60,25 @@ impl App {
             Constraint::Fill(1),
         ]).areas(frame.area());
 
-        frame.render_widget(Block::bordered(), first);
+        let datasets = vec![
+            // Scatter chart
+            Dataset::default()
+                .name("data1")
+                .marker(symbols::Marker::Braille)
+                .graph_type(GraphType::Line)
+                .style(Style::default().cyan())
+                .data(&self.cpu),
+        ];
+
+        let x_axis = Axis::default().bounds([0f64,self.cpu.len() as f64]).style(Style::default().cyan());
+        let y_axis = Axis::default().bounds([0f64,100f64]).style(Style::default().cyan());
+
+        let chart = Chart::new(datasets)
+            .block(Block::bordered().title("CPU"))
+            .x_axis(x_axis)
+            .y_axis(y_axis);
+
+        frame.render_widget(chart, first);
         frame.render_widget(Block::bordered(), second);
         frame.render_widget(Block::bordered(), third);
 
@@ -72,12 +90,14 @@ impl App {
     /// If your application needs to perform work in between handling events, you can use the
     /// [`event::poll`] function to check if there are any events available with a timeout.
     fn handle_crossterm_events(&mut self) -> color_eyre::Result<()> {
-        match event::read()? {
-            // it's important to check KeyEventKind::Press to avoid handling key release events
-            Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
-            Event::Mouse(_) => {}
-            Event::Resize(_, _) => {}
-            _ => {}
+        if event::poll(std::time::Duration::from_millis(16))? {
+            match event::read()? {
+                // it's important to check KeyEventKind::Press to avoid handling key release events
+                Event::Key(key) if key.kind == KeyEventKind::Press => self.on_key_event(key),
+                Event::Mouse(_) => {}
+                Event::Resize(_, _) => {}
+                _ => {}
+            }
         }
         Ok(())
     }
@@ -85,10 +105,10 @@ impl App {
     /// Handles the key events and updates the state of [`App`].
     fn on_key_event(&mut self, key: KeyEvent) {
         match (key.modifiers, key.code) {
-            (_, KeyCode::Esc | KeyCode::Char('q'))
-            | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
-            // Add other key handlers here.
-            _ => {}
+                (_, KeyCode::Esc | KeyCode::Char('q'))
+                | (KeyModifiers::CONTROL, KeyCode::Char('c') | KeyCode::Char('C')) => self.quit(),
+                // Add other key handlers here.
+                _ => {}
         }
     }
 
